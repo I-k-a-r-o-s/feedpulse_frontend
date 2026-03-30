eb6ea6d91757a3f06e2d226f82403bcf63f2092e
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-03-30 16:37:33 +0530

    Basic daisyUI Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-300 shadow-sm">
+      <div className="navbar-start">
+        <a className="btn btn-ghost text-xl">FeedPulse</a>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Admin Login</a>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
