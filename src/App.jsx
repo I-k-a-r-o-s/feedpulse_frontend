97a126aaefb5f0c893989416898d6a6bcaf7e249
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-03-30 17:39:04 +0530

    added react-hot -toast dependancy and added it to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Toaster } from "react-hot-toast";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 
 const App = () => {
   return (
     <div>
+      <Toaster position="bottom-center" reverseOrder={false} />
       <Navbar/>
       <Home />
     </div>
