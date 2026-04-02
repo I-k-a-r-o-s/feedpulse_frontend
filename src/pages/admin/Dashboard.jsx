9aabc1bb8a89c76b058d8cd97977ef3068581114
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-04-02 17:54:43 +0530

    plugged in and designed a basic dashboard

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,16 +1,112 @@
+import { useContext, useEffect, useState } from "react";
+import { appContext } from "../../context/AppContext";
+import toast from "react-hot-toast";
+
 const Dashboard = () => {
+  const { api, loading, setLoading } = useContext(appContext);
+  const [feedback, setFeedback] = useState([]);
+
+  const getAllFeedbacks = async () => {
+    try {
+      setLoading(true);
+      const { data } = await api.get("/api/feedback");
+      if (data.success) {
+        toast.success(data.message);
+        setFeedback(data.allFeedback);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Internal Server Error!");
+      console.log("Error response:", error.response?.data);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const updateStatus = async (id, newStatus) => {
+    try {
+      setLoading(true);
+      const { data } = await api.patch(`/api/feedback/${id}`, {
+        status: newStatus,
+      });
+      if (data.success) {
+        toast.success(data.message);
+        // Update the feedback in the state
+        setFeedback((prev) =>
+          prev.map((item) =>
+            item._id === id ? { ...item, status: newStatus } : item,
+          ),
+        );
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Internal Server Error!");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getAllFeedbacks();
+  }, []);
   return (
     <div className="hero bg-base-200 min-h-[calc(100vh-4rem)]">
       <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Hello there</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button className="btn btn-primary">Get Started</button>
-        </div>
+        {feedback.map((item) => (
+          <div className="card bg-base-100 w-96 shadow-sm" key={item._id}>
+            <div className="card-body">
+              <h2 className="card-title">
+                {item.title ? item.title : "Title"}
+                <div className="badge badge-secondary">
+                  {item.ai_sentiment ? item.ai_sentiment : "Sentiment"}
+                </div>
+              </h2>
+              <p>
+                Description:-
+                {item.description ? item.description : "description"}
+              </p>
+              <p>AI Summary:-{item.ai_summary ? item.ai_summary : "Summary"}</p>
+              <p>
+                Submitter Name:-
+                {item.submitterName ? item.submitterName : "Not Provided"}
+              </p>
+              <p>
+                Submitter Email:-
+                {item.submitterEmail ? item.submitterEmail : "Not Provided"}
+              </p>
+              <fieldset className="fieldset">
+                <legend className="fieldset-legend">
+                  Status:-{item.status}
+                </legend>
+                <select
+                  className="select"
+                  disabled={loading}
+                  onChange={(e) => updateStatus(item._id, e.target.value)}
+                >
+                  <option disabled={true} value="">
+                    {item.status}
+                  </option>
+                  <option value={"New"}>New</option>
+                  <option value={"In Review"}>In Review</option>
+                  <option value={"Resolved"}>Resolved</option>
+                </select>
+              </fieldset>
+              <div className="card-actions justify-end">
+                <div className="badge badge-outline">
+                  Priority:- {item.ai_priority ? item.ai_priority : "Priority"}
+                </div>
+                <div className="badge badge-outline">
+                  Category:- {item.ai_category ? item.ai_category : "Category"}
+                </div>
+                <div className="badge badge-outline">
+                  Date:- {item.updatedAt ? item.updatedAt : "Date"}
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
