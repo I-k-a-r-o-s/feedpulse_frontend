73864f25dececb036e1b62b1bd3872330d7c6833
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-04-02 13:05:20 +0530

    set up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { Toaster } from "react-hot-toast";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import { Route, Routes } from "react-router";
+import Dashboard from "./pages/admin/Dashboard";
 
 const App = () => {
   return (
     <div>
       <Toaster position="bottom-center" reverseOrder={false} />
-      <Navbar/>
-      <Home />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Dashboard />} />
+      </Routes>
     </div>
   );
 };
