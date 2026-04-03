e099b04c0898e9b62a23058a542dc2b69b103d5d
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-04-03 13:22:03 +0530

    added a skeleton when there is no eedback to show

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -54,59 +54,73 @@ const Dashboard = () => {
   return (
     <div className="hero bg-base-200 min-h-[calc(100vh-4rem)]">
       <div className="hero-content text-center">
-        {feedback.map((item) => (
-          <div className="card bg-base-100 w-96 shadow-sm" key={item._id}>
-            <div className="card-body">
-              <h2 className="card-title">
-                {item.title ? item.title : "Title"}
-                <div className="badge badge-secondary">
-                  {item.ai_sentiment ? item.ai_sentiment : "Sentiment"}
-                </div>
-              </h2>
-              <p>
-                Description:-
-                {item.description ? item.description : "description"}
-              </p>
-              <p>AI Summary:-{item.ai_summary ? item.ai_summary : "Summary"}</p>
-              <p>
-                Submitter Name:-
-                {item.submitterName ? item.submitterName : "Not Provided"}
-              </p>
-              <p>
-                Submitter Email:-
-                {item.submitterEmail ? item.submitterEmail : "Not Provided"}
-              </p>
-              <fieldset className="fieldset">
-                <legend className="fieldset-legend">
-                  Status:-{item.status}
-                </legend>
-                <select
-                  className="select"
-                  disabled={loading}
-                  onChange={(e) => updateStatus(item._id, e.target.value)}
-                >
-                  <option disabled={true} value="">
-                    {item.status}
-                  </option>
-                  <option value={"New"}>New</option>
-                  <option value={"In Review"}>In Review</option>
-                  <option value={"Resolved"}>Resolved</option>
-                </select>
-              </fieldset>
-              <div className="card-actions justify-end">
-                <div className="badge badge-outline">
-                  Priority:- {item.ai_priority ? item.ai_priority : "Priority"}
-                </div>
-                <div className="badge badge-outline">
-                  Category:- {item.ai_category ? item.ai_category : "Category"}
-                </div>
-                <div className="badge badge-outline">
-                  Date:- {item.updatedAt ? item.updatedAt : "Date"}
+        {feedback.length > 0 ? (
+          feedback.map((item) => (
+            <div className="card bg-base-100 w-96 shadow-sm" key={item._id}>
+              <div className="card-body">
+                <h2 className="card-title">
+                  {item.title ? item.title : "Title"}
+                  <div className="badge badge-secondary">
+                    {item.ai_sentiment ? item.ai_sentiment : "Sentiment"}
+                  </div>
+                </h2>
+                <p>
+                  Description:-
+                  {item.description ? item.description : "description"}
+                </p>
+                <p>
+                  AI Summary:-{item.ai_summary ? item.ai_summary : "Summary"}
+                </p>
+                <p>
+                  Submitter Name:-
+                  {item.submitterName ? item.submitterName : "Not Provided"}
+                </p>
+                <p>
+                  Submitter Email:-
+                  {item.submitterEmail ? item.submitterEmail : "Not Provided"}
+                </p>
+                <fieldset className="fieldset">
+                  <legend className="fieldset-legend">
+                    Status:-{item.status}
+                  </legend>
+                  <select
+                    className="select"
+                    disabled={loading}
+                    onChange={(e) => updateStatus(item._id, e.target.value)}
+                  >
+                    <option disabled={true} value="">
+                      {item.status}
+                    </option>
+                    <option value={"New"}>New</option>
+                    <option value={"In Review"}>In Review</option>
+                    <option value={"Resolved"}>Resolved</option>
+                  </select>
+                </fieldset>
+                <div className="card-actions justify-end">
+                  <div className="badge badge-outline">
+                    Priority:-{" "}
+                    {item.ai_priority ? item.ai_priority : "Priority"}
+                  </div>
+                  <div className="badge badge-outline">
+                    Category:-{" "}
+                    {item.ai_category ? item.ai_category : "Category"}
+                  </div>
+                  <div className="badge badge-outline">
+                    Date:- {item.updatedAt ? item.updatedAt : "Date"}
+                  </div>
                 </div>
               </div>
             </div>
+          ))
+        ) : (
+          <div className="flex w-52 flex-col gap-4">
+            <div className="skeleton h-32 w-full"></div>
+            <div className="skeleton h-4 w-28"></div>
+            <div className="skeleton h-4 w-full"></div>
+            <div className="skeleton h-4 w-full"></div>
+            <span className="skeleton skeleton-text">No Feedbacks Yet!</span>
           </div>
-        ))}
+        )}
       </div>
     </div>
   );
