e719000f2922c2e2696072a259411328570544af
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-04-03 13:21:31 +0530

    use admin email to determine whether the admin is logged in and display logout button and elements for sorting

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import { VscFeedback } from "react-icons/vsc";
 import AdminLoginModal from "./AdminLoginModal";
 import { Link } from "react-router";
+import { useContext } from "react";
+import { appContext } from "../context/AppContext";
 
 const Navbar = () => {
+  const { admin, logOut } = useContext(appContext);
+
   return (
     <div className="navbar bg-base-300 shadow-sm">
       <div className="navbar-start">
@@ -11,8 +15,59 @@ const Navbar = () => {
           FeedPulse
         </Link>
       </div>
+      {admin ? (
+        <div className="navbar-center">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <details>
+                <summary>Category</summary>
+                <ul className="bg-base-100 rounded-t-none p-2">
+                  <li>
+                    <span>Bug</span>
+                  </li>
+                  <li>
+                    <span>Feature Request</span>
+                  </li>
+                  <li>
+                    <span>Improvement</span>
+                  </li>
+                  <li>
+                    <span>Other</span>
+                  </li>
+                </ul>
+              </details>
+            </li>
+          </ul>
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <details>
+                <summary>Status</summary>
+                <ul className="bg-base-100 rounded-t-none p-2">
+                  <li>
+                    <span>New</span>
+                  </li>
+                  <li>
+                    <span>In Review</span>
+                  </li>
+                  <li>
+                    <span>Resolved</span>
+                  </li>
+                </ul>
+              </details>
+            </li>
+          </ul>
+        </div>
+      ) : (
+        <></>
+      )}
       <div className="navbar-end">
-        <AdminLoginModal />
+        {admin ? (
+          <button className="btn" onClick={logOut}>
+            Logout
+          </button>
+        ) : (
+          <AdminLoginModal />
+        )}
       </div>
     </div>
   );
